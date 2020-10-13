51112b81353c5a60b9b07a58169730e0bbd2df58
Estefania <fanny_pichardo23@hotmail.com>
2020-10-12 19:04:07 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+ console.log("Hola Mundo");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ const nombre='Estefania';
+ const apellido='Pichardo';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//  const nombreCompleto=nombre+' '+apellido;
+
+
+ //Los template strings es para no tener que hacer ese tipo de
+ //concatenaciones clasicas, esta manera es mas facil
+
+ //Esta forma respeta los espacios y saltos de linea
+ const nombreCompleto= `${nombre }  ${apellido} `;
+
+ console.log(nombreCompleto);
+
+ function getSaludo(nombre){
+     
+     return 'Hola ' + nombre;
+ }
+
+ console.log(`Este es un texto ${getSaludo(nombre)}`)
+
+
