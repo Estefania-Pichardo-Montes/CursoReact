76304333d619aaad50914a2052c813298f73f07a
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-11-21 12:00:24 -0600

    Socket

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -4,6 +4,56 @@ const cors= require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
+//Agregamos dependencias  para el web socket
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+//Hacemos el servidor y lo ponemos a escuchar peticiones
+const server= new http.createServer();
+server.listen(webSocketsServerPort);
+console.log('Web socket en puerto 8000');
+
+//Creamos el websocketserver
+const wsServer = new webSocketServer({
+    httpServer:server
+});
+
+//Guardamos los clientes conectados en el ws
+const clients={};
+
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+  };
+
+//funcion para escuchar las peticiones
+wsServer.on('request', (request)=>{
+
+    var userID = getUniqueID();
+    console.log((new Date())+' se recibio una conexion del origen' + request.origin +'.');
+
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+    connection.on('message',(message)=>{
+        
+        if (message.type === 'utf8') {
+            // console.log('Hey');
+            console.log('Received Message: ', message.utf8Data);
+      
+            // broadcasting message to all connected clients
+            for(key in clients) {
+              clients[key].sendUTF(message.utf8Data);
+              console.log('sent Message to: ', clients[key]);
+              
+            }
+        }
+    })
+});
+
+//Aqui termina lo que se agrego para el web socket
 //Crear el servidor de express
 
 const app=express();
